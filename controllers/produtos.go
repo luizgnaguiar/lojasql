13cf3fa7aac301c4b63e6df00bb8481cd111c702
luizgnaguiar <117757696+luizgnaguiar@users.noreply.github.com>
2024-11-11 17:45:26 +0000

    1

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"github/luizgnaguiar/lojasql/models"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -11,5 +13,33 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscarTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversao do preco", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversao da quantidade", err)
+		}
+		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	models.DeletaOProduto(idDoProduto)
+	http.Redirect(w, r, "/", 301)
 
 }
--- models/produtos.go
+++ models/produtos.go
@@ -5,7 +5,7 @@ import "github/luizgnaguiar/lojasql/db"
 type Produto struct {
 	Nome, Descricao string
 	Preco           float64
-	Quantidade, id  int
+	Quantidade, Id  int
 }
 
 func BuscarTodosOsProdutos() []Produto {
@@ -26,6 +26,7 @@ func BuscarTodosOsProdutos() []Produto {
 		if err != nil {
 			panic(err.Error())
 		}
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -35,4 +36,28 @@ func BuscarTodosOsProdutos() []Produto {
 	}
 	defer db.Close()
 	return produtos
+}
+
+func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectarComBancoDeDados()
+
+	insertDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values($1, $2, $3, $4)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insertDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+
+	defer db.Close()
+}
+
+func DeletaOProduto(id string) {
+	db := db.ConectarComBancoDeDados()
+	deletarOProduto, err := db.Prepare("delete from produtos where id=$1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarOProduto.Exec(id)
+	defer db.Close()
 }
